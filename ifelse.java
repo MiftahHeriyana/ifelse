6cdbb33006fb4448faa2eb511a2c5f4f5e0685d4
MiftahHeriyana <miftah09heriyana@gmail.com>
2022-04-08 09:07:11 -0700

    first commit

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+package com.mycompany.tugas;
+
+public class ifelse {
+    public static void main(String[] args){
+        int i;
+        for (i=1; i<=20; i=i+1){
+            if (1%2==0){
+                System.out.print(i+"CCIT");
+            }else{
+                System.out.print(i+"AEU");
+            }
+        }
+    }
+}
